5c32d5881cf92988c9e79586ad049b64e00d142a
Arne Rusek <arne.rusek@showmax.com>
2016-04-29 15:34:58 +0200

    Add timestamps to metrics which are missing them

--- prometheus-pusher.go
+++ prometheus-pusher.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ShowMax/go-fqdn"
@@ -29,6 +31,7 @@ var logger log.Logger
 
 func main() {
 	path := flag.String("config", "/etc/prometheus-pusher/conf.d", "Config file or directory. If directory is specified then all files in the directory will be loaded.")
+	dummy := flag.Bool("dummy", false, "Do not post the metrics, just print them to stdout")
 	flag.Parse()
 
 	logger = log.New("prometheus-pusher")
@@ -43,7 +46,7 @@ func main() {
 
 	for _ = range time.Tick(pusher.PushInterval) {
 		for _, metric := range pusher.Metrics {
-			go getAndPush(metric.Name, metric.URL, pusher.PushGatewayURL, hostname)
+			go getAndPush(metric.Name, metric.URL, pusher.PushGatewayURL, hostname, dummy)
 		}
 	}
 }
@@ -163,21 +166,61 @@ func getMetrics(metricURL string) []byte {
 	return body
 }
 
-func pushMetrics(metricName string, pushgatewayURL string, instance string, metrics []byte) {
+func pushMetrics(metricName string, pushgatewayURL string, instance string, metrics []byte, dummy *bool) {
 	postURL := fmt.Sprintf("%s/metrics/job/%s/instance/%s", pushgatewayURL, metricName, instance)
-	logger.Info("Pushing Node exporter metrics", "endpoint", postURL)
+	if (*dummy) {
+		fmt.Println(string(metrics))
+	} else {
+		logger.Info("Pushing Node exporter metrics", "endpoint", postURL)
 
-	data := bytes.NewReader(metrics)
-	resp, err := http.Post(postURL, "text/plain", data)
-	if err != nil {
-		logger.Error(err.Error(), "error", err)
-		return
+		data := bytes.NewReader(metrics)
+		resp, err := http.Post(postURL, "text/plain", data)
+		if err != nil {
+			logger.Error(err.Error(), "error", err)
+			return
+		}
+		defer resp.Body.Close() // FIXME: no need to close on error?
 	}
-	defer resp.Body.Close()
 }
 
-func getAndPush(metricName string, metricURL string, pushgatewayURL string, instance string) {
+func addTimestamps(metrics []byte) (timestampedMetrics []byte) {
+	/* if the metrics are missing timestams and the pusher stops sending
+	 * for a while, pushgateway will report the same values every time
+	 * prometheus collects it, which results into flat non-zero values which
+	 * are confusing */
+
+	/* Note that this is not optimal and the producers of the data should
+	 * be appending timestamps by themselves. And we will honor them. But
+	 * most of the exporters do not do that unfortunately. */
+
+	currentTime := strconv.Itoa(int(time.Now().Unix()))
+	lines := strings.Split(string(metrics), "\n")
+	for i := 0; i < len(lines); i++ {
+		// skip comments and empty lines
+		if (len(lines[i]) == 0) || (lines[i][0] == '#') {
+			continue
+		}
+		// find closing curly bracket - metrics that have labels
+		lastCBIndex := strings.LastIndex(lines[i], "}")
+		// some metrics do not have labels and curly braces
+		if lastCBIndex == -1 {
+			lastCBIndex = 0
+		}
+		// we'll have "} <value>" for untimestamped but labelled metrics
+		// and "<metric_name> <value>" for untimestamped and unlabelled metrics
+		dataFields := strings.Fields(lines[i][lastCBIndex:])
+		// skip lines that (probably) already have timestamps
+		if len(dataFields) > 2 {
+			continue
+		}
+		lines[i] += " " + currentTime
+	}
+	timestampedMetrics = []byte(strings.Join(lines, "\n"))
+	return
+}
+
+func getAndPush(metricName string, metricURL string, pushgatewayURL string, instance string, dummy *bool) {
 	if metrics := getMetrics(metricURL); metrics != nil {
-		pushMetrics(metricName, pushgatewayURL, instance, metrics)
+		pushMetrics(metricName, pushgatewayURL, instance, addTimestamps(metrics), dummy)
 	}
 }
